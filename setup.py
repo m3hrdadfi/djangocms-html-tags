9626a9a7e8d2396dbdaaf27d79f8f39a4593fde4
Gökmen Görgen <gkmngrgn@gmail.com>
2019-02-12 00:47:48 +0100

    update author information

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from djangocms_html_tags import __version__
 
 
 setup(
-    name='djangocms-html-tags',
+    name="djangocms-html-tags",
     version=__version__,
-    description=open('README.md').read(),
-    author='Gokmen Gorgen',
-    author_email='gokmen@alageek.com',
+    description=open("README.md").read(),
+    author="Gökmen Görgen",
+    author_email="gkmngrgn@gmail.com",
     packages=find_packages(),
-    platforms=['OS Independent'],
+    platforms=["OS Independent"],
     install_requires=[],
     include_package_data=True,
     zip_safe=False,
